24da5e1aaa34df233d203070d64ab4c471f6820c
jcant0n <javiercantonferrero@gmail.com>
2024-07-08 13:50:22 +0200

    Updated the OpenXRGen project to Net 6.0

--- Generate-Bindings.ps1
+++ Generate-Bindings.ps1
@@ -42,7 +42,7 @@ else
 
 # Run generator
 LogDebug "START binding generator process"
-pushd .\OpenXRGen\OpenXRGen\bin\Release\netcoreapp3.1
+pushd .\OpenXRGen\OpenXRGen\bin\Release\net6.0
 .\publish\OpenXRGen.exe
 if($?)
 {
